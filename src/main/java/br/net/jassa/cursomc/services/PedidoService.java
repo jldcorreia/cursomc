a945be97feeaceec38f43b3d36d75c141021a0dc
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-12 13:48:15 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/net/jassa/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package br.net.jassa.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.net.jassa.cursomc.domain.Pedido;
+import br.net.jassa.cursomc.repositories.PedidoRepository;
+import br.net.jassa.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+
+	}
+
+}
