bcbcf6ec9f0b0974e16ee6b6cefe1e366b0f7fd7
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-13 11:53:06 -0300

    Listando todas categorias com DTO

--- src/main/java/br/net/jassa/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/net/jassa/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package br.net.jassa.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.net.jassa.cursomc.domain.Categoria;
+import br.net.jassa.cursomc.dto.CategoriaDTO;
 import br.net.jassa.cursomc.services.CategoriaService;
 
 @RestController
@@ -21,6 +24,19 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		/* Padrão DTO -> cria uma outra classe para apenas trabalhar com os dados da classe original
+		 * P. Ex. Categoria traz os produtos da categoria, mas não queremos, então criamos outra classe CategoriaDTO
+		 * apenas com os dados que queremos mostrar e lá na classe CategoriaDTO o construtor recebe o Categoria */
+		List<CategoriaDTO> listDto = list.stream()
+				                         .map(obj -> new CategoriaDTO(obj)) 
+				                         .collect(Collectors.toList());
+		return ResponseEntity.ok()
+				             .body(listDto);
+	}	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
