6150ed88e62f1223e76681714ecdb3e80b7cc6b3
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-13 11:10:30 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/br/net/jassa/cursomc/resources/ClienteResource.java
+++ src/main/java/br/net/jassa/cursomc/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.net.jassa.cursomc.domain.Categoria;
 import br.net.jassa.cursomc.domain.Cliente;
 import br.net.jassa.cursomc.services.ClienteService;
 
@@ -19,8 +20,8 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Cliente obj = service.buscar(id);
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
