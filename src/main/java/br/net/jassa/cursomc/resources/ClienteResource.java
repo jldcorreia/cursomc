8f46991b41c71774b3465b1449c4331dd1d71f69
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-14 09:48:05 -0300

    Inserindo um novo cliente com POST

--- src/main/java/br/net/jassa/cursomc/resources/ClienteResource.java
+++ src/main/java/br/net/jassa/cursomc/resources/ClienteResource.java
@@ -1,6 +1,7 @@
 package br.net.jassa.cursomc.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.net.jassa.cursomc.domain.Cliente;
 import br.net.jassa.cursomc.dto.ClienteDTO;
+import br.net.jassa.cursomc.dto.ClienteNewDTO;
 import br.net.jassa.cursomc.services.ClienteService;
 
 @RestController
@@ -32,6 +35,18 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				       .fromCurrentRequest()
+				       .path("/{id}")
+				       .buildAndExpand(obj.getId())
+				       .toUri();
+		return ResponseEntity.created(uri).build();
+	}	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
--- src/main/java/br/net/jassa/cursomc/services/ClienteService.java
+++ src/main/java/br/net/jassa/cursomc/services/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.net.jassa.cursomc.domain.Cidade;
 import br.net.jassa.cursomc.domain.Cliente;
+import br.net.jassa.cursomc.domain.Endereco;
+import br.net.jassa.cursomc.domain.enums.TipoCliente;
 import br.net.jassa.cursomc.dto.ClienteDTO;
+import br.net.jassa.cursomc.dto.ClienteNewDTO;
+import br.net.jassa.cursomc.repositories.CidadeRepository;
 import br.net.jassa.cursomc.repositories.ClienteRepository;
+import br.net.jassa.cursomc.repositories.EnderecoRepository;
 import br.net.jassa.cursomc.services.exceptions.DataIntegrityException;
 import br.net.jassa.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,12 +27,22 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;	
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-
+	}	
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}	
 	
 	public Cliente update (Cliente obj) {
@@ -57,6 +73,21 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Optional<Cidade> aux = cidadeRepository.findById(objDto.getCidadeId());
+		Cidade cid = null;
+		if (aux.isPresent()) {
+			cid = aux.get();
+		}
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) cli.getTelefones().add(objDto.getTelefone2());
+		if (objDto.getTelefone3()!=null) cli.getTelefones().add(objDto.getTelefone3());
+		return cli;
+	}	
 	
 	/* rotina que atualiza o dto para o objeto gerenciado do BD */
 	private void updateData(Cliente newObj, Cliente obj) {
