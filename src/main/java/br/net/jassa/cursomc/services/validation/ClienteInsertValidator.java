fd440a95c9a3f688b8936faac171aa1fb0cd4eae
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-14 13:49:10 -0300

    Validação customizada: e-mail não repetido na inserção de cliente

--- src/main/java/br/net/jassa/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/net/jassa/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.net.jassa.cursomc.domain.Cliente;
 import br.net.jassa.cursomc.domain.enums.TipoCliente;
 import br.net.jassa.cursomc.dto.ClienteNewDTO;
+import br.net.jassa.cursomc.repositories.ClienteRepository;
 import br.net.jassa.cursomc.resources.exception.FieldMessage;
 import br.net.jassa.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux!=null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		// vai pegar todos os objetos de erros e incluir no contexto do framework
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
