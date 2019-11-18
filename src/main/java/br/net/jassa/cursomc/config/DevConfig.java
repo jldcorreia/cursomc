59e2d2f88ec948368a43c94683da432ec874d836
Jacinto Leonardo <jacinto.leonardo@gmail.com>
2019-11-18 16:09:53 -0300

    Criando profile de Desenvolvimento

---
+++ src/main/java/br/net/jassa/cursomc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package br.net.jassa.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.net.jassa.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	/* essa forma busca o parametro no arquivo application.properties e associa ao valor da variavel strategy */
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		/* se a estrategia é de criar o BDs roda os comandos sql */
+		if (strategy.equals("create")) {
+			dbService.instantiateTestDatabase();
+		}
+		return true;
+	}
+}
